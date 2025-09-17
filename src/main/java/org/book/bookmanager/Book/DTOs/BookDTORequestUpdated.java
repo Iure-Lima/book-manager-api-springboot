9979e527878c5e75536b49584d29ed1c9b56ee68
IureLima <iurelimasilva@gmail.com>
2025-09-17 16:16:48 -0300

    feat: add dtos for book

---
+++ src/main/java/org/book/bookmanager/Book/DTOs/BookDTORequestUpdated.java
@@ -0,0 +1,27 @@
+package org.book.bookmanager.Book.DTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.book.bookmanager.Book.Enum.BookCategory;
+import org.book.bookmanager.Book.Enum.BookStatus;
+
+import java.util.List;
+
+public record BookDTORequestUpdated(
+        String bookTitle,
+        String bookSubtitle,
+        String bookAuthor,
+        String bookPublisher,
+        Integer bookYear,
+        Integer bookEdition,
+        String bookLanguage,
+        Integer bookPageNumber,
+        List<BookCategory> bookCategories,
+        String bookSynopsis,
+        String bookBannerURL,
+        Integer bookQuantityInStock,
+        BookStatus bookStatus,
+        List<String> bookKeywords,
+        Double bookPopularity,
+        Double bookReviews
+) { }
