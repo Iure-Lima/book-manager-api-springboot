41c777eccd185a4dfe0340a93c29dd09586f1e8d
IureLima <iurelimasilva@gmail.com>
2025-09-17 11:21:47 -0300

    feat: create the main structure for the project

---
+++ src/test/java/org/book/bookmanager/BookManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.book.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
