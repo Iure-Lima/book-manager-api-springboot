82764546f5422b5b0ada69196e6c822cd18263e3
IureLima <iurelimasilva@gmail.com>
2025-09-17 17:50:05 -0300

    Fix: error in the token configuration

--- src/main/java/org/book/bookmanager/configs/security/SecurityConfiguration.java
+++ src/main/java/org/book/bookmanager/configs/security/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
-                    authorize.requestMatchers(HttpMethod.POST, "/book").hasRole("LIBRARIAN");
+                    authorize.requestMatchers(HttpMethod.POST, "/book").hasAnyRole("ADMIN", "LIBRARIAN");
                     authorize.anyRequest().authenticated();
                 }).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
