42636f7ae502fc5d15df9c1671e7748d5c26d098
IureLima <iurelimasilva@gmail.com>
2025-09-17 15:56:51 -0300

    refactore: modify the repository for update the finds

--- src/main/java/org/book/bookmanager/Book/Repositories/BookRepository.java
+++ src/main/java/org/book/bookmanager/Book/Repositories/BookRepository.java
@@ -4,10 +4,12 @@ import org.book.bookmanager.Book.Enum.BookCategory;
 import org.book.bookmanager.Book.Enum.BookStatus;
 import org.book.bookmanager.Book.Model.BookModel;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
+
+import java.util.Collection;
 import java.util.List;
 
 @Repository
@@ -16,10 +18,15 @@ public interface BookRepository extends MongoRepository<BookModel, String> {
     BookModel findByBookTitle(String bookTitle);
     BookModel findByBookISBN(String bookISBN);
 
-    Page<BookModel> findByBookCategories(List<BookCategory> bookCategories, Pageable pageable);
-    Page<BookModel> findByBookKeywords(List<String> bookKeywords, Pageable pageable);
-    Page<BookModel> findByBookAuthors(String bookAuthors, Pageable pageable);
     Page<BookModel> findAll(Pageable pageable);
+
+    Page<BookModel> findByBookTitleContainingIgnoreCase(String title, Pageable pageable);
+    Page<BookModel> findByBookAuthorsContainingIgnoreCase(String bookAuthors, Pageable pageable);
+
+    Page<BookModel> findByBookCategoriesIn(Collection<List<BookCategory>> bookCategories, Pageable pageable);
+    Page<BookModel> findByBookKeywordsIn(Collection<List<String>> bookKeywords, Pageable pageable);
+    Page<BookModel> findByBookAuthorsIn(List<String> authors, Pageable pageable);
+
     Page<BookModel> findByBookYear(Integer bookYear, Pageable pageable);
     Page<BookModel> findByBookLanguage(String bookLanguage, Pageable pageable);
     Page<BookModel> findByBookStatus(BookStatus bookStatus, Pageable pageable);
