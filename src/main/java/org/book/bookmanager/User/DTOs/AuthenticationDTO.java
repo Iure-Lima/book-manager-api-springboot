06db0a9b935e4560637a0418c8ad468120293793
IureLima <iurelimasilva@gmail.com>
2025-09-17 13:04:35 -0300

    feat: add the dtos for the project

---
+++ src/main/java/org/book/bookmanager/User/DTOs/AuthenticationDTO.java
@@ -0,0 +1,9 @@
+package org.book.bookmanager.User.DTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record AuthenticationDTO(
+        @NotNull @NotBlank String email,
+        @NotNull @NotBlank String password
+) { }
---
+++ src/main/java/org/book/bookmanager/User/DTOs/RegisterDTO.java
@@ -0,0 +1,12 @@
+package org.book.bookmanager.User.DTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.book.bookmanager.User.Enum.UserRole;
+
+public record RegisterDTO(
+        @NotNull @NotNull String email,
+        @NotNull @NotBlank String password,
+        @NotNull @NotBlank String username,
+        @NotNull UserRole userRole
+        ) {}
