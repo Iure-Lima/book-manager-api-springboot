2b99e53a25de61fa48a910d17664df68d2f2415b
IureLima <iurelimasilva@gmail.com>
2025-09-17 23:12:36 -0300

    Feat: add method in the enum for validation

--- src/main/java/org/book/bookmanager/Book/Enum/BookSort.java
+++ src/main/java/org/book/bookmanager/Book/Enum/BookSort.java
@@ -14,4 +14,13 @@ public enum BookSort {
     BookSort(String bookSort){
         this.bookSort = bookSort;
     }
+
+    public static boolean isValid(String value) {
+        for (BookSort sort : BookSort.values()) {
+            if (sort.getBookSort().equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
