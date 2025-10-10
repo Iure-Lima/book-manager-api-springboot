b4b3dcecd341a179c1a81ffa4422ab2745f9310c
IureLima <iurelimasilva@gmail.com>
2025-10-10 17:12:16 -0300

    feat: add wishList model

---
+++ src/main/java/org/book/bookmanager/WishList/Model/WishListModel.java
@@ -0,0 +1,39 @@
+package org.book.bookmanager.WishList.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "wishlist")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishListModel {
+    @MongoId
+    private String wishlistId;
+
+    @Field(name="name")
+    private String wishlistName;
+
+    @Field(name = "userlogin")
+    @Indexed
+    private String userLogin;
+
+    @Field(name="books")
+    private String[] booksId;
+
+    @Field(name="createAt")
+    private LocalDateTime createAt;
+
+    @Field(name = "updateAt")
+    private LocalDateTime updateAt;
+
+}
